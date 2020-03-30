600aefee766a45304eecc4e1808d8adbf4efeb09
Roktim Reza <1405081-rmrr@users.noreply.github.com>
2020-03-30 12:20:41 +0600

    Buffer er jhamela

---
+++ BufferClass.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class BufferClass {
+    public  static void main(String[]args)
+    {
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(in);
+        try {
+            //String name = br.readLine();
+            //System.out.println(name);
+            String name = "";
+            while(!(name.equals("stop")))
+            {
+                 name = br.readLine();
+                System.out.println(name);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
